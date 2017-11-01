e51c12a21fcf3e565104d0dbdb3b97c37269290a
847490239 <847490239@qq.com>
2017-11-01 17:09:47 +0800

    '2'

--- main.py
+++ main.py
@@ -6,13 +6,18 @@ import tornado.ioloop
 import tornado.options
 import tornado.web
 import torndb
+import logging
 import json
 import api.ip2city.ipip
 
 from tornado.options import define, options
 
 define("port", default=10086, help="run on the given port", type=int)
-
+logging.basicConfig(level=logging.error,
+                    format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S',
+                    filename='myapp.log',
+                    filemode='w')
 
 def _load_ip_to_city():
     api.ip2city.ipip.IP.load("api/data/17monipdb20170909.dat")
@@ -48,11 +53,11 @@ class IndexHandler(tornado.web.RequestHandler):
 class InterfaceHandler(tornado.web.RequestHandler):
     def get(self):
         listads = []
-        listads[0]=[]
-        listads[1] = []
-        listads[2] = []
-        listads[3] = []
-        listads[4] = []
+        listads0 = []
+        listads1 = []
+        listads2 = []
+        listads3 = []
+        listads4 = []
         # 获取时间
         self.time = time.strftime('%Y-%m-%d', time.localtime(time.time()))
         # 连接数据库
@@ -78,32 +83,63 @@ class InterfaceHandler(tornado.web.RequestHandler):
         # print(self.planid)
 
         ads = db.query(
-            'select	zyads_plan.top,zyads_plan.expire,zyads_ads.planid,adinfo,imageurl,imageurl1,url,headline,description FROM zyads_ads inner join zyads_plan on zyads_ads.planid=zyads_plan.planid where zyads_ads.status=3 and zyads_plan.status=1 AND zyads_ads.adstypeid=13  order by zyads_plan.priceadv desc')
+            'select	zyads_plan.top,zyads_ads.adstypeid,zyads_plan.expire,zyads_ads.planid,zyads_ads.uid,zyads_ads.adsid,adinfo,imageurl,imageurl1,url,headline,description FROM zyads_ads inner join zyads_plan on zyads_ads.planid=zyads_plan.planid where zyads_ads.status=3 and zyads_plan.status=1 AND zyads_ads.adstypeid=13  order by zyads_plan.priceadv desc')
 
         for val in ads:
+
             if str(val['expire']) < self.time:
                 continue
             if {'planid':val['planid']} in self.planid:
                 continue
             val['expire'] = ''
-            if val['top'] == '110':
-                listads[1].append(val)
-            if val['top'] == '111':
-                listads[2].append(val)
-            if val['top'] == '112':
-                listads[3].append(val)
-            if val['top'] == '113':
-                listads[4].append(val)
-            listads[0].append(val)
-        # self.render("poem.html", html=listads)
-        listads = json.dumps(listads)
-        self.write(listads)
+            if val['top'] == 110:
+                print ('110')
+                listads1.append(val)
+            if val['top'] == 111:
+                print ('111')
+                listads2.append(val)
+            if val['top'] == 112:
+                print ('112')
+                listads3.append(val)
+            if val['top'] == 113:
+                print ('113')
+                listads4.append(val)
 
+            listads0.append(val)
+        # self.render("poem.html", html=listads)
+        listads = [listads0,listads1,listads2,listads3,listads4]
+        # self.render("poem.html", html=listads)
+        # listads = json.dumps(listads)
+        # self.write(listads)
+        self.render("poem.html", html=listads)
+
+class redirectHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.name = self.get_argument('url')
+        self.adsid = self.get_argument('adsid')
+        self.adstypeid = self.get_argument('adstypeid')
+        self.planid = self.get_argument('planid')
+        self.uid = self.get_argument('uid')
+        self.write(self.name)
+        # self.write(self.adsid)
+        db = torndb.Connection('127.0.0.1:3306', 'zygg', 'root', 'root')
+        # 获取时间
+        self.time = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+        try:
+            aaa = db.query('select clicks from zyads_stats WHERE  day=%s AND adsid=%s',self.time,self.adsid)
+            if aaa :
+                db.execute('update zyads_stats set clicks=clicks+1 WHERE  day=%s AND adsid=%s',self.time,self.adsid)
+            else:
+                db.execute('insert into zyads_stats (clicks,adsid,day,adstypeid,planid,uid) VALUES (1,%s,%s,%s,%s,%s)',self.adsid,self.time,self.adstypeid,self.planid,self.uid)
+        except Exception as e:
+            logging.error(e)
+        finally:
+            self.redirect(self.name)
 
 if __name__ == '__main__':
     tornado.options.parse_command_line()
     app = tornado.web.Application(
-        handlers=[(r'/', IndexHandler),(r'/ads', InterfaceHandler)],
+        handlers=[(r'/', IndexHandler),(r'/ads', InterfaceHandler),(r'/redi', redirectHandler)],
         template_path=os.path.join(os.path.dirname(__file__), "views"),
         static_path=os.path.join(os.path.dirname(__file__), "static"),
         debug=True
--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 aaa = [1,2,3]
 for val in [1,2,3,4]:
    print val
-
+logging.basicConfig(level=logging.WARNING,
+                    format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S',
+                    filename='myapp.log',
+                    filemode='w')
